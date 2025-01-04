23c4685b1c0b20c7af01afe160f65bf994d14062
Sunil Behera <sunil.6318ie@gmail.com>
2025-01-04 23:31:45 +0530

    Add formik and change to form tag

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -13,18 +13,18 @@ const Login = () => {
             backgroundRepeat: 'no-repeat', // Prevents repeating
             height: '100vh', // Full height of the viewport
         }}>
-          <div className="w-full sm:w-3/12 min-w-96 py-5 px-10 bg-black/70 rounded-sm">
+          <form className="w-full sm:w-3/12 min-w-96 py-5 px-10 bg-black/70 rounded-sm">
             <h1 className="text-3xl font-bold mb-8">Sign In</h1>
             <div className="w-full mt-4">
                 <label className="block text-sm/6 font-medium">Email</label>
                 <div className="mt-2">
-                    <input type="email" name="email" id="email" className="block w-full py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6 rounded-sm" placeholder="Email" />
+                    <input type="email" name="email" id="email" className="block w-full py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6 rounded-sm bg-slate-300" placeholder="Email" />
                 </div>
             </div>
             <div className="w-full mt-4">
                 <label className="block text-sm/6 font-medium">Password</label>
                 <div className="mt-2">
-                    <input type="password" name="password" id="password" className="block w-full py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6 rounded-sm" placeholder="Password" />
+                    <input type="password" name="password" id="password" className="block w-full py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6 rounded-sm bg-slate-300" placeholder="Password" />
                 </div>
             </div>
             <div className="w-full mt-4 text-center">
@@ -32,7 +32,7 @@ const Login = () => {
                 <h2 className="my-4">Forgot Password?</h2>
                 <h2>New to Netflix? <Link to="/">Sign Up</Link></h2>
             </div>
-          </div>
+          </form>
         </div>
     )
 };
